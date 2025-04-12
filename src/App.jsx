ece79a10214e8cbb6c7288b3b1bc7e98f10bd57f
Zakaria Maalim <olyblv@gmail.com>
2025-04-12 21:10:48 +0300

    majority of the project is done
    the styles, and the components of the portfolio are done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+/**
+ * components
+ */
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
+
+/**
+ * styles
+ */
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
